e8e2411b0d31d3717978b172e3c25998667db115
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2024-02-02 19:45:24 +0800

    02.02

---
+++ db.js
@@ -0,0 +1,18 @@
+const postgres = require('postgres');
+require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: 'require',
+    connection: {
+        options: `project=${ENDPOINT_ID}`,
+    },
+});
+
+exports.sql = sql;
